689a7de94f51e18816b01d35f484470a39605caa
Ponmani05 <ponnumani0506@gmail.com>
2023-01-22 17:13:48 +0530

    login page created.

---
+++ src/components/Auth/index.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react"
+import { useLocation,useNavigate  } from "react-router-dom"
+import "./style.css";
+
+export default function (props) {
+    const navigate = useNavigate();
+    const { pathname } = useLocation()
+    const [authMode, setAuthMode] = useState("")
+    
+
+    useEffect(() => {
+        changeAuthMode(pathname === '/signup' ? 'signup' : 'login')
+    }, [pathname])
+
+    const changeAuthMode = (url) => {
+        setAuthMode(url)
+    }
+
+    if (authMode === "login") {
+        return (
+            <div className="Auth-form-container">
+                <form className="Auth-form">
+                    <div className="Auth-form-content">
+                        <h3 className="Auth-form-title">Sign In</h3>
+                        <div className="text-center">
+                            Not registered yet?{" "}
+                            <span className="link-primary" onClick={() =>navigate('/signup')}>
+                                Sign Up
+                            </span>
+                        </div>
+                        <div className="form-group mt-3">
+                            <label>Email address</label>
+                            <input
+                                type="email"
+                                className="form-control mt-1"
+                                placeholder="Enter email"
+                            />
+                        </div>
+                        <div className="form-group mt-3">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                className="form-control mt-1"
+                                placeholder="Enter password"
+                            />
+                        </div>
+                        <div className="d-grid gap-2 mt-3">
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
+                        </div>
+                        <p className="text-center mt-2">
+                            Forgot <a href="#">password?</a>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+    return (
+        <div className="Auth-form-container">
+            <form className="Auth-form">
+                <div className="Auth-form-content">
+                    <h3 className="Auth-form-title">Sign In</h3>
+                    <div className="text-center">
+                        Already registered?{" "}
+                        <span className="link-primary" onClick={() => navigate('/')}>
+                            Sign In
+                        </span>
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Full Name</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="e.g Jane Doe"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="Email Address"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Password"
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                    <p className="text-center mt-2">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </div>
+            </form>
+        </div>
+    )
+}
